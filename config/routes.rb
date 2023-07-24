9eb3a76fe069de93c960b41dad46b8584f3d9bd0
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2023-07-23 21:57:42 -0300

    config DB and generate Tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
